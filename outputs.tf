92106dc9b86b6d05dd1e23072ae187905c94a195
govi06s <dukkagovinda8179@gmail.com>
2025-04-01 19:46:15 +0530

    terraform-aws-vpc

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,18 @@ output "vpc_id" {
 
 }
 
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
+
 # output "az_info" {
 #   value = data.aws_availability_zones.available
 # }
